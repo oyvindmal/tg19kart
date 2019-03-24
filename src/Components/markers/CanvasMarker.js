5f4fa686c1e18eabdb732d195f73b8555ef8c127
Øyvind Malin <oyvindmalin@oyvindmal.onmicrosoft.com>
2019-03-24 19:37:13 +0100

    wtf

--- src/Components/markers/CanvasMarker.js
+++ src/Components/markers/CanvasMarker.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import {Rect,Text, Group} from 'react-konva';
+import Dialog from '@material-ui/core/Dialog';
+import { Paper, Typography, DialogTitle, List, ListItem, ListItemText } from '@material-ui/core';
+
 
 class CanvasMarker extends Component {
     constructor(props) {
         super(props);
-       
+        this.state = {
+            clickedMarker: null
+
+        }
     
     }
 
@@ -18,14 +24,17 @@ class CanvasMarker extends Component {
 
     detailsClick = (object) => {
         console.log(object);
+        this.setState({clickedMarker : object})
     }
   render() {
-
+    console.log(this.state);
     return (
+       
         <Group>
             <Rect onClick={() => this.detailsClick(this.props.object)} x={this.props.x} y={this.props.y} fill={this.getColor(this.props.v)} width={30} height={30}  stroke="black" strokeWidth={2}/>
         <Text onClick={() => this.detailsClick(this.props.object)} text={this.props.a} x={this.props.x + 5} y={this.props.y+10} fontStyle="bold" fill="white" />
         </Group>
+  
     );
   }
 }
