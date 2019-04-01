d53acf34a1c83076b65e7627a8203facfc0c31ac
Øyvind Malin <oyvindmalin@oyvindmal.onmicrosoft.com>
2019-04-02 00:14:17 +0200

    New marker types

---
+++ src/Components/markers/AreaMarker.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {Rect,Text, Group} from 'react-konva';
+
+class AreaMarker extends Component {
+    constructor(props) {
+        super(props);
+    
+    }
+
+
+  render() {
+  
+    return (
+        <Group>
+        <Rect x={this.props.x} y={this.props.y} fill={this.props.fill} width={this.props.w} height={this.props.h}  stroke="black" strokeWidth={0}/>
+    <Text  text={this.props.description} x={this.props.x + 5} width={this.props.w} height={this.props.h} y={this.props.y+10} fontStyle="bold" fontSize={25} fill="white" />
+    </Group>
+    );
+  }
+}
+
+export default AreaMarker;
